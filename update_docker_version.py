96bb93e2e577c8ffb322664580268b570d0b7db4
Weby Homelab <homelab@weby.srvrs.top>
2026-02-27 08:26:01 +0200

    Cleanup and sync v1.9.5

--- update_docker_version.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('/root/geminicli/flash-monitor-kyiv/docker-compose.yml', 'r') as f:
-    text = f.read()
-
-new_text = re.sub(r'image: webyhomelab/flash-monitor-kyiv:v[0-9.]+', 'image: webyhomelab/flash-monitor-kyiv:v1.9.4', text)
-
-with open('/root/geminicli/flash-monitor-kyiv/docker-compose.yml', 'w') as f:
-    f.write(new_text)
